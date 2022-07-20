2668222a1da8b80a3f5529597d4c47f0710ce80c
10846012 <102201397+10846012@users.noreply.github.com>
2022-07-20 11:06:13 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from cgitb import text
+import re
 from flask import Flask, request, abort
 
 from linebot import (
@@ -14,7 +15,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('c9U4RITsjcLHDX14yfulJar4huzOEVHoiGBzjb1df3rNvE1humv+HoobUT0BoXYWfgtQTKIJhKaOq3+7ererZS0z+t23f1TvNMbTym9WZiF9Qxs7zICPwudGi9By1v9rumuCgXdxDX7dySQ+5RtSFgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('8ae0e5241c8b31b78b8361a25711d527')
 
-line_bot_api.push_message('Uf5b29d10f1347f1ce7d0f256d3f1b4fd', TextSendMessage(text='你可以開始了'))
+line_bot_api.push_message('Uf5b29d10f1347f1ce7d0f256d3f1b4fd', TextSendMessage(text='請輸入"視訊"'))
 
 
 @app.route("/callback", methods=['POST'])
@@ -40,7 +41,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = text=event.message.text
-    if(message == '連接'):
+    if re.match('視訊',message):
         flex_message = FlexSendMessage(
             alt_text='連接點選',
             contents={
